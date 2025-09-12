b18d7b026e48bd4dc73992f6c55c441fdd8dc849
GiannKim <giannk@nycstudents.net>
2025-09-12 14:03:33 +0000

    Print name and ewewaddress

---
+++ Lesson-03b-Classwork/Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+    System.out.println("Giann Kim");
+    System.out.println("Giann Kim");
+    System.out.println("41-26 210th");
+    System.out.println("Giann Kim \n41-26 210th st");
+  }
+
+  
+}
