fe45c483df945f729bdd5a80e07b46e94b65a634
GiannKim <giannk@nycstudents.net>
2025-09-25 14:13:35 +0000

    update

---
+++ Lesson-07-activity/Main.java
@@ -0,0 +1,100 @@
+
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+/*  
+    Challenge 1:
+    1) Create the variables, ask the user for the variable values, write the equation in file EQ1-act6 and display the equation value.
+*/
+    System.out.println("enter x");
+    double x = Input.readDouble();
+    double y = 0;
+    y = Math.pow(x,7);
+    System.out.println(y);
+
+/*  
+    Challenge 2:
+    1) Create the variables, ask the user for the variable values, write the equation in fileEQ1.1-act6 and display the equation value.
+*/
+    
+    System.out.println("enter z");
+    double z  = Input.readDouble();
+    double q = 0;
+    q = Math.pow(z,3) + 5;
+    System.out.println(q);
+
+/*  
+    Challenge 3:
+    Create the variables, ask the user for the variable values, write the equation in file EQ2-act6 and display the equation value..
+    
+*/
+
+    System.out.println("enter t");
+    double t = Input.readDouble();
+    System.out.println("enter r");
+    double r = Input.readDouble();
+    double s = 0;
+    s = Math.pow(t,5)* Math.pow((r + 2),4);
+    System.out.println(s);
+
+/*  
+    Challenge 4:
+    Create the variables, ask the user for the variable values, write the equation in file EQ3-act6 and display the equation value..
+    
+*/
+
+    System.out.println("enter A");
+    double A = Input.readDouble();
+    System.out.println("enter B");
+    double B = Input.readDouble();
+    double C = 0;
+    C = Math.sqrt(A + B);
+    System.out.println(C);
+
+/*  
+    Challenge 5:
+    Create the variables, ask the user for the variable values, write the equation in file EQ4-act6 and display the equation value..
+    
+*/
+
+
+
+
+/*  
+    Challenge 6:
+    Create the variables, ask the user for the variable values, write the equation g=sin(deg) and display the equation value..
+    
+*/
+
+
+
+
+
+/*  
+    Challenge 7:
+    Create the variables, ask the user for the variable values, write the equation in file EQ5-act6 and display the equation value.
+    
+*/
+
+
+
+
+/*  
+    *** Bonus Challenge ***:
+    Create the variables, ask the user for the variable values, write the equation in file Ch-act6 and display the equation value.
+
+    HINT: What does the "plus minus: after "-b" mean.
+*/
+
+
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
