bd794f331da02476824272737f8e6bd57c6576e2
GiannKim <giannk@nycstudents.net>
2025-11-18 01:38:42 +0000

    w

---
+++ Lesson-22-Classwork/Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+    String[] friends = {Joe, Tom, James, Rudolph, Mary}
+   
+    
+  }
+
+  
+
+  double celsiusToFarhenheit(double c){
+        return c*9/5+32;
+  }
+}
