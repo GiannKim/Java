6eb61edc0d8e5d9675dd5eec892fb64af03ac963
GiannKim <giannk@nycstudents.net>
2026-01-08 02:04:41 +0000

    w

---
+++ Lesson-29-Classwork/Main.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+    Dog dog1 = new Dog();
+    dog1.name = “Love”;
+    dog1.age = 2;
+    dog1.breed = “Shiba Inu”;
+
+  }
+
+
+
+
+  
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+}
